dc49df484cc2c739d6151e44b02178ff463f16ba
sandeepa <sanvidurath98@gmail.com>
2025-03-27 12:56:52 +0530

    finished add-customer

---
+++ FrontEnd/src/app/models/Customer.ts
@@ -0,0 +1,11 @@
+export class Customer {
+  name: string;
+  address: string;
+  salary: number;
+
+  constructor(name: string, address: string, salary: number) {
+    this.name = name;
+    this.address = address;
+    this.salary = salary;
+  }
+}
--- FrontEnd/src/app/pages/add-customer/add-customer.component.ts
+++ FrontEnd/src/app/pages/add-customer/add-customer.component.ts
@@ -1,11 +1,55 @@
 import { Component } from '@angular/core';
+import { Customer } from '../../models/Customer';
 
 @Component({
   selector: 'app-add-customer',
   imports: [],
   templateUrl: './add-customer.component.html',
-  styleUrl: './add-customer.component.css'
+  styleUrl: './add-customer.component.css',
 })
 export class AddCustomerComponent {
+  submitForm() {
+    const name = (
+      document.getElementById('name') as HTMLInputElement
+    ).value.trim();
+    const address = (
+      document.getElementById('address') as HTMLInputElement
+    ).value.trim();
+    const salary = parseFloat((
+      document.getElementById('salary') as HTMLInputElement
+    ).value.trim());
 
+    if (!(name || address || salary)) {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    if (salary) {
+      alert('Please enter a proper number');
+      return;
+    }
+
+    const customerData: Customer = { name, address, salary };
+    // const customerData: Customer = new Customer( name, address, salary );
+
+    fetch('http://localhost:8080/customer/add', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(customerData),
+    })
+    .then((response: Response) => {
+      console.log(response);
+      console.log(response.text);
+      if (response.status === 201) {  // Check if customer was successfully created
+        return alert('Customer added successfully!');
+      } else {
+        return response.text().then(errorMessage => {
+          throw new Error(errorMessage || 'Failed to add customer');
+        });
+      }
+    })
+      .catch((err) => alert(err));
+  }
 }
